0b5a971b5fb490e91dbb1d42cc84deee55b2e387
Frédéric KOLLER <koller.f@gmail.com>
2020-09-08 07:30:08 +0200

    fix eslint

--- src/reta-ui/hoc/withCommonProps.js
+++ src/reta-ui/hoc/withCommonProps.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { pick, propOr } from "ramda";
-import classnames from "classnames";
+import React from 'react';
+import { pick, propOr } from 'ramda';
+import classnames from 'classnames';
 import {
   generateCommonClasses,
   generateClassesWithReplaceKey,
   generateClassesAndAddedKeyName,
-} from "../tools/classGenerator";
+} from '../tools/classGenerator';
 
 const withCommonProps = (Component) => {
   const component = (props) => {
     const commonProps = pick([
-      "p",
-      "pt",
-      "pb",
-      "pr",
-      "pl",
-      "px",
-      "py",
-      "m",
-      "mt",
-      "mb",
-      "mr",
-      "ml",
-      "mx",
-      "my",
-      "bg",
-      "font",
+      'p',
+      'pt',
+      'pb',
+      'pr',
+      'pl',
+      'px',
+      'py',
+      'm',
+      'mt',
+      'mb',
+      'mr',
+      'ml',
+      'mx',
+      'my',
+      'bg',
+      'font',
     ])(props);
     const classNames = classnames(
-      propOr(null, "className", props),
+      propOr(null, 'className', props),
       generateCommonClasses(commonProps),
-      generateClassesWithReplaceKey(/color/g, pick(["textcolor"], props)),
-      generateClassesWithReplaceKey(/size/g, pick(["textsize"], props)),
-      generateClassesAndAddedKeyName("border", props)
+      generateClassesWithReplaceKey(/color/g, pick(['textcolor'], props)),
+      generateClassesWithReplaceKey(/size/g, pick(['textsize'], props)),
+      generateClassesAndAddedKeyName('border', props)
     );
 
     return <Component className={classNames} {...props} />;
@@ -43,6 +43,6 @@ const withCommonProps = (Component) => {
 };
 
 const getDisplayName = (Component) => {
-  return Component.displayName || Component.name || "unamed component";
+  return Component.displayName || Component.name || 'unamed component';
 };
 export default withCommonProps;
