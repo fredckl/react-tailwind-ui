f3e51064ead174b70447511dc5bbd9197be01eff
Frédéric KOLLER <koller.f@gmail.com>
2020-09-08 07:30:18 +0200

    add proptypes

--- src/reta-ui/text/Text.js
+++ src/reta-ui/text/Text.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { withCommonProps } from '../hoc';
+import PropTypes from 'prop-types';
 
 const Text = ({
   as = 'p',
   children,
   ...restProps
-}) => React.createElement(as, restProps, children)
+}) => React.createElement(as, restProps, children);
+
+Text.propTypes = {
+  as: PropTypes.string
+}
 
 export default withCommonProps(Text);
